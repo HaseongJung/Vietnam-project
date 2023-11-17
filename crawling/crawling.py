a2163170cda1f779e90391104d3dfbf4aafd6c58
Jung-HaSeong <haseong@gmail.com>
2023-11-18 03:12:15 +0900

    crawling

--- crawling/crawling.py
+++ crawling/crawling.py
@@ -23,17 +23,32 @@ headers = {
 
 
 def setRestaurantPages(url: str):
-    url = url
+    url = url   
     response = requests.get(url, headers=headers)
     response.raise_for_status()  # 200번대 코드가 아니면 코드가 멈춤
-    print("response successful!")
+    print("Response successful!")
     html = response.text
     soup = BeautifulSoup(html, "html.parser")
+
     restraurant_pages = []
-    for restraurant_page in soup.find_all('div', class_="biGQs _P fiohW alXOW NwcxK GzNcM ytVPx UTQMg RnEEZ ngXxk"):
-        restraurant_pages.append("https://www.tripadvisor.com/" + restraurant_page.find('a')['href'])
-    # print(restraurant_pages)
-    print(f'total restraurant pages: {len(restraurant_pages)}')
+    total_restraurants = int(soup.find_all('span', class_="b")[1].text)
+    main_pages = total_restraurants//30 if total_restraurants%30==0 else total_restraurants//30+1
+    for i in tqdm(range(main_pages-1), desc="Set restaurant pages"):
+        if i > 0:
+            current_url = url[:48] + f'oa{i*30}' + url[47:]
+            # print(f'current_url: {current_url}')
+            response = requests.get(current_url, headers=headers)
+            response.raise_for_status()
+            html = response.text
+            soup = BeautifulSoup(html, "html.parser")
+
+        for restraurant_page in soup.find_all('div', class_="biGQs _P fiohW alXOW NwcxK GzNcM ytVPx UTQMg RnEEZ ngXxk"):
+            restraurant_pages.append("https://www.tripadvisor.com/" + restraurant_page.find('a')['href'])
+            # print(restraurant_page.find('a')['href'])
+        time.sleep
+        # print(f'Total restraurant pages: {len(restraurant_pages)}')
+        
+    print(f'Total restraurant pages: {len(restraurant_pages)}')
     
     return restraurant_pages
 
@@ -42,41 +57,42 @@ def createDataset():
         "name": [],
         "category": [],
         "description": [],
-        "category": [],
         "opneingTime": [],
         "closingTime": [],
         "URL": [],
         "priceLow": [],
         "priceHigh": [],
         "reviews": [],
+        "adress": [],
+        "rating": []
     })
     
     return dataset
 
 def setWebdriver():
-    driver = webdriver.Chrome(ChromeDriverManager().install())
     options = Options()
     options.add_argument('user-agent=' + user_agent)
-    # options.add_argument('headless') #headless모드 브라우저가 뜨지 않고 실행됩니다.
+    options.add_argument('headless') #headless모드 브라우저가 뜨지 않고 실행됩니다.
     # options.add_argument('--window-size= x, y') #실행되는 브라우저 크기를 지정할 수 있습니다.
     # options.add_argument('--start-maximized') #브라우저가 최대화된 상태로 실행됩니다.
     # options.add_argument('--start-fullscreen') #브라우저가 풀스크린 모드(F11)로 실행됩니다.
     # options.add_argument('--blink-settings=imagesEnabled=false') #브라우저에서 이미지 로딩을 하지 않습니다.
     options.add_argument('--mute-audio') #브라우저에 음소거 옵션을 적용합니다.
     options.add_argument('incognito') #시크릿 모드의 브라우저가 실행됩니다.
-    driver.set_window_size(1920, 1080)
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
     return driver
 
-def buildDataset(dataset: str, pages: list):
-    for restaurant_page in tqdm(pages, desc="restaurants..."):
+def buildDataset(dataset, pages: list):
+    cnt = 0
+    for restaurant_page in tqdm(pages, desc="Restaurants"):
         response = requests.get(restaurant_page, headers=headers)
         response.raise_for_status()
         html = response.text
         soup = BeautifulSoup(html, "html.parser")
 
         # name 
-        name = soup.find('h1', class_="HjBfq").text;    print(f'name: {name}')
+        name = soup.find('h1', class_="HjBfq").text;    print(f'Name: {name}')
 
         # category
         category = ''
@@ -84,19 +100,20 @@ def buildDataset(dataset: str, pages: list):
             if '$' not in x.text:
                 category += f'{x.text}, '
         category = category[:-2]
-        print(f'category: {category}')
+        print(f'Category: {category}')
 
         # adress
-        adress = soup.find('a', class_="AYHFM").text;   print(f'adress: {adress}')
+        adress = soup.find_all('a', class_="AYHFM")[1].text;   print(f'Adress: {adress}')
 
         # rating
-        rating = soup.find('span', class_="ZDEqb").text;    print(f'rating: {rating}')
+        rating = soup.find('span', class_="ZDEqb").text;    print(f'Rating: {rating}')
 
         # time
-        openingTime = soup.find('span', class_="mMkhr").text[11:19];    print(f'openingTime: {openingTime}')
-        closingTime = soup.find('span', class_="mMkhr").text[22:30];    print(f'closingTime: {closingTime}')
+        openingTime = soup.find('span', class_="mMkhr").text[11:19];    
+        closingTime = soup.find('span', class_="mMkhr").text[22:30];   
+        print(f'OpeningTime: {openingTime}, ClosingTime: {closingTime}')
 
-        url = restaurant_page;    print(f'url: {url}')
+        url = restaurant_page;    print(f'URL: {url}')
 
         # price
         try:
@@ -104,59 +121,52 @@ def buildDataset(dataset: str, pages: list):
             priceHigh = soup.find('div', class_="SrqKb").text.split(' - ')[1]
         except:
             priceLow = np.NaN; priceHigh = np.NaN
-        print (f'priceLow: {priceLow}, priceHigh: {priceHigh}')
+        print (f'PriceLow: {priceLow}, PriceHigh: {priceHigh}')
 
         # reviews, only english reviews
-        total_reviews = int(soup.find('span', class_="count").text[1:-1].replace(',', ''));    print(f'total_reviews: {total_reviews}')
-        review_pages = total_reviews // 15 if total_reviews%15 == 0 else total_reviews//15 + 1;    print(f'review_pages: {review_pages}')
-        for i in tqdm(range(1, review_pages), desc='review pages...'):
+        total_reviews = int(soup.find_all('span', class_="count")[0].text[1:-1].replace(',', ''));    print(f'Total_reviews: {total_reviews}')
+        review_pages = total_reviews//15 if total_reviews%15 == 0 else total_reviews//15 + 1;    print(f'Review pages: {review_pages}')
+        review_list = []
+        for i in tqdm(range(1, review_pages), desc='Review crwaling...'):
             if i > 1:
-                url_list = url.split('-Reviews')
-                url_list.insert(1, f'-or{i*15}')
-                current_url = ''.join(url_list)
+                url_list = url.split('-Reviews-')
+                current_url = url_list[0] + f'-Reviews-or{15*i}-' + url_list[1]
                 # print(f'current_url: {current_url}')
                 response = requests.get(current_url, headers=headers)
                 response.raise_for_status()
                 html = response.text
-                soup = BeautifulSoup(html, "html.parser")
+                soup = BeautifulSoup(html, "html.parser") 
 
-            review = []
             reviews = soup.find_all('p', class_="partial_entry");
             for review in reviews:
-                review.append(review.text)
+                review_list.append(review.text)
             #     print(review.text)
+            # print(f'Reviews: {len(review_list)}')
 
             time.sleep(rd.uniform(0.1, 0.5))
+        print(f'Reviews: {len(review_list)}')
 
         # description
         driver.get(url)
         try:
-            WebDriverWait(driver, 10).until(
+            WebDriverWait(driver, 5).until(
                     EC.presence_of_element_located((By.CSS_SELECTOR, "#component_52 > div.hILIJ > div > div:nth-child(2) > div > div > div.gmbZC > a")))
             element = driver.find_element(By.CSS_SELECTOR, "#component_52 > div.hILIJ > div > div:nth-child(2) > div > div > div.gmbZC > a")
             element.click()
-            WebDriverWait(driver, 5).until(
+            WebDriverWait(driver, 3).until(
                     EC.presence_of_element_located((By.CSS_SELECTOR, "#BODY_BLOCK_JQUERY_REFLOW > div.VZmgo.D.X0.X1.Za > div > div.TocEc._Z.S2.H2._f > div > div > div.kwVln > div > div:nth-child(1) > div > div.jmnaM")))
             element = driver.find_element(By.CSS_SELECTOR, "#BODY_BLOCK_JQUERY_REFLOW > div.VZmgo.D.X0.X1.Za > div > div.TocEc._Z.S2.H2._f > div > div > div.kwVln > div > div:nth-child(1) > div > div.jmnaM")
             description = element.text
             print(description)
         except:
             description = np.NaN
-        print(f'description: {description}\n')
-        
+        print(f'Description: {description}\n')
         
         time.sleep(rd.uniform(1, 2))
-        dataset["name"] = [name]
-        dataset["category"] = [category]
-        dataset["adress"] = [adress]
-        dataset["rating"] = [rating]
-        dataset["opneingTime"] = [openingTime]
-        dataset["closingTime"] = [closingTime]
-        dataset["URL"] = [url]
-        dataset["priceLow"] = [priceLow]
-        dataset["priceHigh"] = [priceHigh]
-        dataset["reviews"] = [reviews]
-        dataset
+
+        dataset.loc[cnt] = [name, category, description, openingTime, closingTime, url, priceLow, priceHigh, review_list, adress, rating]
+        cnt += 1
+        
         
     return dataset 
 
@@ -176,3 +186,4 @@ if __name__ == "__main__":
 
     # save dataset
     dataset.to_csv('test.csv')
+    print(f'Dataset build complete!\n{dataset}')
